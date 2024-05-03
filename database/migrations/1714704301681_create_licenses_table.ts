d47fdcad4b3b9f122f3aa5627722d18f1d63310e
koirherianto <koir.harianto@gmail.com>
2024-05-03 10:52:37 +0800

    licenses migration

---
+++ database/migrations/1714704301681_create_licenses_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'licenses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().notNullable()
+      table.string('name', 255).notNullable()
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at')
+      table.timestamp('deleted_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
