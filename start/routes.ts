4b84dd75ccd799f21886c5e3c624ba8e09dea62d
koirherianto <koir.harianto@gmail.com>
2024-05-05 19:05:29 +0800

    add hello word

--- start/routes.ts
+++ start/routes.ts
@@ -4,6 +4,10 @@ import { middleware } from '#start/kernel'
 const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
 
+router.get('/', async () => {
+  return { message: 'Hello world' }
+})
+
 router.group(() => {
   router.post('/register', [AuthController, 'register'])
   router.post('/login', [AuthController, 'login'])
