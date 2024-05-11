e8d1a9ce477d44c065bab77870b4c8869d5d5ade
koirherianto <koir.harianto@gmail.com>
2024-05-11 20:31:43 +0800

    test licenses update

--- tests/functional/licenses/update.spec.ts
+++ tests/functional/licenses/update.spec.ts
@@ -2,7 +2,7 @@ import License from '#models/license'
 import { createTestLicense, createTestUser, removeTestLicense, removeTestUser, TestUserResult } from '#tests/util'
 import { test } from '@japa/runner'
 
-test.group('Licenses show | GET | api/licenses/:id', async (group) => {
+test.group('Licenses Update API | PUT | api/licenses', async (group) => {
   let testUser: TestUserResult | null = null
   let testLicense: License | null = null
 
@@ -15,9 +15,10 @@ test.group('Licenses show | GET | api/licenses/:id', async (group) => {
     await removeTestLicense()
     await removeTestUser()
   })
-  test('should can get Licnese', async ({ client, expect }) => {
-    const response = await client.get('/api/licenses/' + testLicense!.id)
+  test('Update an licenses', async ({ client, expect }) => {
+    const response = await client.put('/api/licenses/' + testLicense?.id)
       .bearerToken(testUser!.token)
+      .json({name: 'test license'})
 
     const body = response.body()
 
@@ -25,23 +26,36 @@ test.group('Licenses show | GET | api/licenses/:id', async (group) => {
     expect(body.success).toBeTruthy()
     expect(body.data.id).toBeDefined()
     expect(body.data.name).toBe('test license')
-    expect(body.message).toBe('Get license data successfully')
+    expect(body.message).toBe('License updated successfully')
   })
 
   test('should return 422 if license not found', async ({ client, expect }) => {
-    const response = await client.get('/api/licenses/' + '1234-1234-1234-1234') // invalid uuid
+    const response = await client.put('/api/licenses/' + '1234-1234-1234-1234') // invalid uuid
       .bearerToken(testUser!.token)
+      .json({name: 'test license'})
 
     expect(response.status()).toBe(422)
     expect(response.body().errors).toBeDefined()
   })
 
-  test('should return 401 if token is wrong', async ({ client, expect }) => {
-    const response = await client.get('/api/licenses/' + testLicense!.id)
+  test('Reject if name is empty', async ({ client, expect }) => {
+    const response = await client.put('/api/licenses/' + testLicense?.id)
+      .bearerToken(testUser!.token)
+      .json({name: ''})
+
+    expect(response.status()).toBe(422)
+    expect(response.body().errors).toBeDefined()
+  })
+
+  test('Reject if token is wrong', async ({ client, expect }) => {
+    const response = await client.put('/api/licenses/' + testLicense?.id)
       .bearerToken('wrong token')
+      .json({name: 'test licens'})
+    
+    const body = response.body()
 
     expect(response.status()).toBe(401)
-    expect(response.body().errors).toBeDefined()
+    expect(body.errors).toBeDefined()
   })
 
 })
