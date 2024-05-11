10d25c759dc7ba5bf0e0ff9f2ee0ab5e4fcfa929
koirherianto <koir.harianto@gmail.com>
2024-05-11 13:20:19 +0800

    licenses index test

--- tests/functional/licenses/index.spec.ts
+++ tests/functional/licenses/index.spec.ts
@@ -1,6 +1,36 @@
+import { createTestLicense, createTestUser, removeTestLicense, removeTestUser, TestUserResult } from '#tests/util'
 import { test } from '@japa/runner'
 
-test.group('Licenses index', () => {
-  test('example test', async ({ assert }) => {
+test.group('Licenses index API | GET | api/licenses', async (group) => {
+  let testUser: TestUserResult | null = null
+
+  group.each.setup(async () => {
+    testUser = await createTestUser()
+    await createTestLicense()
+  })
+
+  group.each.teardown(async () => {
+    await removeTestLicense()
+    await removeTestUser()
+  })
+
+  test('should return all licenses', async ({ client, expect }) => {
+    const response = await client.get('/api/licenses')
+      .bearerToken(testUser!.token)
+
+    console.log(response.body());
+
+    expect(response.status()).toBe(200)
+    expect(response.body().success).toBeTruthy()
+    expect(response.body().data.length).toBeGreaterThan(0)
+    expect(response.body().message).toBeDefined()
+  })
+
+  test('should return 401 if token is wrong', async ({ client, expect }) => {
+    const response = await client.get('/api/licenses')
+      .bearerToken('wrong token!')
+
+    expect(response.status()).toBe(401)
+    expect(response.body().errors).toBeDefined()
   })
 })
