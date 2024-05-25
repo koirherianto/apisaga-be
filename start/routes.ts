4c5681fb1ca8dee6ca9d4ed8f27df9dfe9a285ed
koirherianto <koir.harianto@gmail.com>
2024-05-25 19:55:11 +0800

    file

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,7 @@ const VersionsController = () => import('#controllers/versions_controller')
 const SidebarSeparator = () => import('#controllers/sidebar_separators_controller')
 const SidebarItem = () => import('#controllers/sidebar_items_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return { message: 'Hello world' }
@@ -25,14 +26,19 @@ router.group(() => {
   })
 
   router.group(() => {
-    router.get('/me', [AuthController, 'me'])
-    router.delete('/logout', [AuthController, 'logout'])
+    // apakah bisa di akses tanpa token
+    router.get('/me', [AuthController, 'me']) //no
+    router.delete('/logout', [AuthController, 'logout']) //no
 
+    // tidak bisa diakses tanpa token
+    // untuk mendapatkan data dari licen harus terhubung ke project
     router.resource('/licenses', LicensesController)
+
+    
     router.resource('/projects', ProjectsController)
 
-    router.get('/projects/:slug/version', [VersionsController, 'index'])
-    router.get('/projects/:slug/version/:version', [VersionsController, 'show'])
+    router.get('/projects/:slug/version', [VersionsController, 'index']) //yes
+    router.get('/projects/:slug/version/:version', [VersionsController, 'show']) //yes
     router.post('/projects/:slug/version', [VersionsController, 'store'])
     router.put('/projects/:slug/version/:version', [VersionsController, 'update'])
     router.delete('/projects/:slug/version/:version', [VersionsController, 'destroy'])
@@ -55,8 +61,7 @@ router.group(() => {
     router.post('/projects/:projectSlug/version/:version/sidebar-separators/:separatorSlug/sidebar-items/:itemSlug', [SidebarItem, 'attachToSeparator'])
     router.delete('/projects/:projectSlug/version/:version/sidebar-separators/:separatorSlug/sidebar-items/:itemSlug', [SidebarItem, 'detachFromSeparator'])
 
-  })
-  // }).use(middleware.auth())
+  }).use(middleware.auth())
 
   
 }).prefix('api')
